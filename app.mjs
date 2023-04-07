3dd28dff1a362c7d9b180cc4c48247d3a00fa618
Glenda Boeker <glendaboeker@Glendas-MBP.lan>
2023-04-07 00:54:19 -0400

    deployed

--- app.mjs
+++ app.mjs
@@ -17,11 +17,41 @@ app.use(express.urlencoded({extended: false})); //
 
 
 
+// app.get('/', (req, res) => {
+//     res.render('addCard');
+// });
+  
+// app.post('/', (req, res) => {
+//     const d = new Deck({
+//         question: req.body.question
+//     });
+//     d.save()
+//         .then(() => res.redirect('/'))
+//         .catch(() => res.status(500).send('server error'));
+  
+// });
+
 app.get('/', (req, res) => {
-    res.render('addCard');
+    const query = {};
+    const foundQuestions = req.query.question;
+    if(foundQuestions){
+      query.question = foundQuestions;
+    }
+    Deck.find(query) 
+      .then(foundCards => {
+        res.render('table', {foundCards});
+      })
+      .catch(() => res.status(500).send('server error'));
+      
 });
   
-app.post('/', (req, res) => {
+  
+  
+app.get('/cards/add', (req, res) => {
+    res.render("addCard");
+  });
+
+app.post('/cards/add', (req, res) => {
     const d = new Deck({
         question: req.body.question
     });
